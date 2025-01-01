a7821122095df9c5ac8dc3c4e491cdfc052491b9
Doc <65479309+DocilElm@users.noreply.github.com>
2025-01-01 12:51:20 -0700

    effects: add `UIEffect`

---
+++ src/main/kotlin/com/github/synnerz/talium/effects/UIEffect.kt
@@ -0,0 +1,29 @@
+package com.github.synnerz.talium.effects
+
+import com.github.synnerz.talium.components.UIBase
+
+open class UIEffect @JvmOverloads constructor(
+    val component: UIBase? = null
+) {
+    /**
+     * * Whether this effect adds its own color to the stack or not
+     * * i.e. if i want this effect to color the component red i'd set this to `true`
+     */
+    open var forceColor: Boolean = false
+    /**
+     * * Override this method if you need to do something **before** the component is drawn
+     */
+    open fun preDraw() {}
+    /**
+     * * Override this method if you need to do something **after** the component is drawn
+     */
+    open fun postDraw() {}
+    /**
+     * * Override this method if you need to do something **before** the children are drawn
+     */
+    open fun preChildDraw() {}
+    /**
+     * * Override this method if you need to do something **after** the children are drawn
+     */
+    open fun postChildDraw() {}
+}
