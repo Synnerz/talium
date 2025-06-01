097fb03a6a078779a75a706bb08a0efbada3ea25
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-01 00:51:43 -0700

    comp: add `UIItem`

---
+++ src/main/kotlin/com/github/synnerz/talium/components/UIItem.kt
@@ -0,0 +1,40 @@
+package com.github.synnerz.talium.components
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.client.renderer.RenderHelper
+import net.minecraft.item.ItemStack
+import java.awt.Color
+
+open class UIItem @JvmOverloads constructor(
+    _x: Double,
+    _y: Double,
+    _width: Double,
+    _height: Double,
+    var item: ItemStack,
+    var zlevel: Float = 200f,
+    var scale: Float = 1f,
+    parent: UIBase?
+) : UIBase(_x, _y, _width, _height, parent) {
+    override var bgColor: Color = Color(255, 255, 255, 255)
+
+    override fun render() {
+        val itemRenderer = Minecraft.getMinecraft().renderItem
+
+        if (scale != 1f) {
+            GlStateManager.scale(scale, scale, 1f)
+        }
+
+        GlStateManager.enableTexture2D()
+        RenderHelper.enableStandardItemLighting()
+        RenderHelper.enableGUIStandardItemLighting()
+        itemRenderer.zLevel = zlevel
+        itemRenderer.renderItemIntoGUI(item, (x / scale).toInt(), (y / scale).toInt())
+        RenderHelper.disableStandardItemLighting()
+        GlStateManager.disableTexture2D()
+
+        if (scale != 1f) {
+            GlStateManager.scale(1f / scale, 1f / scale, 1f)
+        }
+    }
+}
