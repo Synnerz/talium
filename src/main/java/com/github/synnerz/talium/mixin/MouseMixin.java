a3e75acab4f629ec43f39df8758c15c134ef4b2e
Doc <65479309+DocilElm@users.noreply.github.com>
2026-07-12 22:03:39 -0700

    internal: higher prio mixin

--- src/main/java/com/github/synnerz/talium/mixin/MouseMixin.java
+++ src/main/java/com/github/synnerz/talium/mixin/MouseMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(MouseHandler.class)
+@Mixin(value = MouseHandler.class, priority = 1003)
 public class MouseMixin {
     @Inject(
             method = "onScroll",
