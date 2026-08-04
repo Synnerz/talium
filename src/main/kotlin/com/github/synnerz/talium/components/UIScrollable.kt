d42a68bdf144ab84c56b5dbd8b84efa4b3cd6112
Doc <65479309+DocilElm@users.noreply.github.com>
2026-08-04 09:37:53 -0700

    internal: remove debug messages

--- src/main/kotlin/com/github/synnerz/talium/components/UIScrollable.kt
+++ src/main/kotlin/com/github/synnerz/talium/components/UIScrollable.kt
@@ -83,12 +83,10 @@ open class UIScrollable @JvmOverloads constructor(
     }
 
     override fun <T : UIMouseEvent> modifyChildMouseEvent(event: T) {
-        println("yOffset(+${yOffset})")
         event.y += yOffset
     }
 
     override fun <T : UIMouseEvent> resetChildMouseEvent(event: T) {
-        println("yOffset(-${yOffset})")
         event.y -= yOffset
     }
 }
